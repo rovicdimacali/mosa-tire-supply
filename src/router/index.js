24dd8d64ec5375eeb49a1a72f465b200264d16d9
Rovic <rovic_luna19@yahoo.com>
2024-02-27 21:09:31 +0800

    Added vue template

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from "vue-router";
+import AppLayout from "../views/pages/AppLayout.vue";
+import Home from "../views/applayouts/Home.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      component: AppLayout,
+      children: [
+        {
+          path: "/home",
+          name: "Home",
+          component: Home,
+        },
+      ],
+    },
+  ],
+});
+
+export default router;
