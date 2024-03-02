a6078772ad4659f2b6897b97ca3757d14c8b0d42
Rovic <rovic_luna19@yahoo.com>
2024-03-02 22:01:37 +0800

    Added copyright and redirects file

--- src/components/home/section/Footer/Footer.vue
+++ src/components/home/section/Footer/Footer.vue
@@ -1,43 +1,46 @@
 <template>
-  <div class="footer row">
-    <div class="footer-info col">
-      <div class="col">
-        <img
-          src="https://res.cloudinary.com/dpm5vdakr/image/upload/v1709128297/mosawebapp/assets/Untitled_design_1_k6o8rv.png"
-          alt=""
-          class="logo"
-        />
-        <small class="about-us">
-          Established with a mission to redefine your driving experience, Mosa
-          Tire Shop brings together expertise, top-notch products, and a
-          friendly atmosphere. Whether you're in need of tire replacements,
-          alignments, or routine maintenance, our skilled team of professionals
-          is dedicated to ensuring your safety and maximizing the lifespan of
-          your tires.
-        </small>
+  <div class="footer col">
+    <div class="footer-row row">
+      <div class="footer-info col">
+        <div class="col">
+          <img
+            src="https://res.cloudinary.com/dpm5vdakr/image/upload/v1709128297/mosawebapp/assets/Untitled_design_1_k6o8rv.png"
+            alt=""
+            class="logo"
+          />
+          <small class="about-us">
+            Established with a mission to redefine your driving experience, Mosa
+            Tire Shop brings together expertise, top-notch products, and a
+            friendly atmosphere. Whether you're in need of tire replacements,
+            alignments, or routine maintenance, our skilled team of
+            professionals is dedicated to ensuring your safety and maximizing
+            the lifespan of your tires.
+          </small>
+        </div>
       </div>
-    </div>
-    <div class="footer-info col">
-      <div class="col">
-        <p>Email</p>
-        <small>mosatiresupply@gmail.com </small>
-      </div>
-      <div class="col">
-        <p>Address</p>
-        <small>#07 Quirino Highway Barangay Pasong Putik, Quezon City </small>
-      </div>
-      <div class="col">
-        <p>Social Media</p>
-        <div class="socmed row">
-          <a href="https://www.facebook.com/MosaTires" target="_blank"
-            ><Icon icon="bi:facebook" class="socmed-icon"
-          /></a>
-          <a href="https://www.instagram.com/mosatiresupply/" target="_blank"
-            ><Icon icon="bi:instagram" class="socmed-icon"
-          /></a>
+      <div class="footer-info col">
+        <div class="col">
+          <p>Email</p>
+          <small>mosatiresupply@gmail.com </small>
+        </div>
+        <div class="col">
+          <p>Address</p>
+          <small>#07 Quirino Highway Barangay Pasong Putik, Quezon City </small>
+        </div>
+        <div class="col">
+          <p>Social Media</p>
+          <div class="socmed row">
+            <a href="https://www.facebook.com/MosaTires" target="_blank"
+              ><Icon icon="bi:facebook" class="socmed-icon"
+            /></a>
+            <a href="https://www.instagram.com/mosatiresupply/" target="_blank"
+              ><Icon icon="bi:instagram" class="socmed-icon"
+            /></a>
+          </div>
         </div>
       </div>
     </div>
+    <small class="copyright">© Copyright 2024. All Rights Reserved.</small>
   </div>
 </template>
 
