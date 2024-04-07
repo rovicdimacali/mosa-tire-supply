50b59c6b0d4504a7215372349f9ec743e623ba55
Rovic <joserovic.dimacali@gmail.com>
2024-04-07 19:35:11 +0800

    Fixed products preview

--- src/components/home/section/Brands/ProductsPreview.vue
+++ src/components/home/section/Brands/ProductsPreview.vue
@@ -2,7 +2,7 @@
   <div class="products-preview col">
     <Carousel
       :value="products"
-      :numVisible="4"
+      :numVisible="this.products.length > 4 ? 4 : this.products.length"
       :numScroll="1"
       :responsiveOptions="responsiveOptions"
       circular
