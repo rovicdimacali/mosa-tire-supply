30c9edc7d088f3614d99d0809e007c7752a92c5a
Rovic <joserovic.dimacali@gmail.com>
2024-05-11 20:53:19 +0800

    Added polling of fetching brands every 10 seconds

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
   <RouterView />
 </template>
+
 <script>
-export default {};
+import { getBrands } from "./services/Admin/Products";
+
+export default {
+  data() {
+    return {
+      pollingInterval: null,
+    };
+  },
+  methods: {
+    async fetchBrandsPolling() {
+      try {
+        await getBrands();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  mounted() {
+    // Fetch brands immediately when the component is mounted
+    this.fetchBrandsPolling();
+
+    // Polling every 10 seconds
+    this.pollingInterval = setInterval(() => {
+      this.fetchBrandsPolling();
+    }, 10000); // 10 seconds in milliseconds
+  },
+  beforeDestroy() {
+    clearInterval(this.pollingInterval);
+  },
+};
 </script>
